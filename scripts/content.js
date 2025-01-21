f6183073155163286d3996e5b73e4b26d4a1ab55
Israel blasbalg <Israelbls12@gmail.com>
2025-01-21 14:47:50 +0200

    implemented a function to itarate over all the thread

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,82 @@
+async function getPostsFromThread(threadUrl) {
+    let posts = [];// an array of all posts in the thread
+
+
+    let currentPageUrl = threadUrl; // the url of the current page
+
+    let firstPageThreadURL = threadUrl.split('/page-')[0] + "/"; // the url of the first page of the thread
+
+    let pageNumber = 1; // the number of the current page
+
+    // fetch the first page content
+    let response = await fetch(firstPageThreadURL);
+    let html = await response.text();
+    let parser = new DOMParser();
+    let document = parser.parseFromString(html, 'text/html');
+    posts.push({pageNumber : `page-${pageNumber}` , posts : getPostsFromPage(document)});
+
+    while (true) {
+        let nextPageContent = await getNextPageContent(currentPageUrl);
+
+        // if the next page content is null then break the loop
+        if (nextPageContent === null) {
+            break;
+        }
+
+        pageNumber++;
+        currentPageUrl = nextPageContent[1]; // set the current page url to the next page url
+        document = nextPageContent[0]; // set the current page document to the next page document
+        posts.push({pageNumber : `page-${pageNumber}` , posts : getPostsFromPage(document)});
+    }
+
+    return posts;
+}
+
+async function getNextPageContent(currentPageUrl) {
+    let currentPage = currentPageUrl.split('/').pop(); // get the current page number
+
+    // if the current page is the first page (without a number) then set it to 1
+    if (!currentPage.includes('page')) {
+        currentPage = "page-1";
+        currentPageUrl += `${currentPage}`;
+    }
+
+    // get the next page number
+    let nextPage = parseInt(currentPage[currentPage.length - 1]) + 1;
+    const nextPageUrl = currentPageUrl.replace(currentPage, `page-${nextPage}`);
+
+    // fetch the next page content
+    const response = await fetch(nextPageUrl);
+
+    // if the response is ok and not redirected
+    if (response.status === 200 && response.redirected === false) {
+        const html = await response.text();
+        const parser = new DOMParser();
+        const document = parser.parseFromString(html, 'text/html');
+        return [document, nextPageUrl]; // return the document and the url of the next page
+    } else {
+        return null; // return null if the response is not ok
+    }
+}
+
 function getPostsFromPage() {
-    let posts = [];// an array of all posts in the page
+    let posts = []; // an array of all posts in the page
+
+    // get all posts elements in the page
     let postsElements = document.querySelectorAll('article[class*="message--post"]');
 
     postsElements.forEach((postElement) => {
+        // get the auoter name and level
         let auoterName = postElement.querySelector('.message-name a span').textContent;
         let auoterLevel = postElement.querySelector('h5[class*="userTitle"]').textContent;
 
+        // create an auoter object with the data
         let auoter = new Auoter(auoterName, auoterLevel);
 
+        // get the post content
         let postContent = postElement.querySelector('.bbWrapper');
 
+        // get all qoutes in the post
         let qoutes = [];
         if (postContent.querySelector('blockquote')) {
             const qoutePosts = postContent.querySelectorAll('blockquote');
@@ -17,11 +84,11 @@ function getPostsFromPage() {
                 let qoutePostContent = qoutePost.querySelector('div[class*="bbCodeBlock-expandContent"]').textContent;
                 let qoutePostAuoterName = qoutePost.querySelector('a').textContent;
                 qoutes.push(new QoutePost(qoutePostAuoterName, qoutePostContent, null));
-                qoutePost.parentNode.removeChild(qoutePost);
+                qoutePost.parentNode.removeChild(qoutePost); // remove the qoute from the post content
             });
         }
 
-        let postText = postContent.textContent; // the text of the post
+        let postText = postContent.textContent; // the text cntent of the post
 
         let postDate = postElement.querySelector('time').dateTime; // the date of the post
 
@@ -31,6 +98,7 @@ function getPostsFromPage() {
 
         posts.push(post);
     });
+
     return posts;
 }
 
@@ -52,5 +120,3 @@ function Auoter(name, level) {
     this.name = name;
     this.level = level;
 }
-console.log("test");
-console.log(getPostsFromPage());
